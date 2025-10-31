0a087683feb182ce66bcad9f3431f8c2a11e33fa
William Zhu <zhuhuilin@users.noreply.github.com>
2025-10-31 15:08:59 -0700

    Initial Commit

---
+++ WakeOnDemand/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WakeOnDemand
+//
+//  Created by Huilin Zhu on 10/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WakeOnDemand/WakeOnDemandApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakeOnDemandApp.swift
+//  WakeOnDemand
+//
+//  Created by Huilin Zhu on 10/31/25.
+//
+
+import SwiftUI
+
+@main
+struct WakeOnDemandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
